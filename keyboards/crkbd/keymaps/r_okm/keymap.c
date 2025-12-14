e45832d4433f204e41f8866af4a9814d977522a7
r-okm <65703649+r-okm@users.noreply.github.com>
2025-12-14 20:10:19 +0900

    keymap: tweak modifier placements for main layer
    - changed `LALT_T(KC_TAB)` to plain `KC_TAB` at the start position
    - made leftmost thumb key `LALT_T(JP_MHEN)` for better access
    - updated `KEYMAP.JSN` and `keymap.c` for consistency

--- keyboards/crkbd/keymaps/r_okm/keymap.c
+++ keyboards/crkbd/keymaps/r_okm/keymap.c
@@ -30,13 +30,13 @@ enum layer_NUMBER {
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   [_QWERTY] = LAYOUT_split_3x6_3(
   //,-----------------------------------------------------.                    ,-----------------------------------------------------.
-LALT_T(KC_TAB),   KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,                         KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,   JP_AT,
+       KC_TAB,    KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,                         KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,   JP_AT,
   //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
       KC_LCTL,    KC_A,    KC_S,    KC_D,    KC_F,    KC_G,                         KC_H,    KC_J,    KC_K,    KC_L, JP_SCLN, JP_COLN,
   //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
       KC_LSFT,    KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,                         KC_N,    KC_M, JP_COMM,  JP_DOT, JP_SLSH, JP_BSLS,
   //|--------+--------+--------+--------+--------+--------+--------|  |--------+--------+--------+--------+--------+--------+--------|
-                                           JP_MHEN, MO(_FN), KC_SPC,   KC_ENT, MO(_NUMBER), LGUI_T(JP_HENK)
+                                  LALT_T(JP_MHEN), MO(_FN), KC_SPC,     KC_ENT, MO(_NUMBER), LGUI_T(JP_HENK)
                                       //`--------------------------'  `--------------------------'
   ),
 
